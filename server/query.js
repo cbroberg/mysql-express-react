56459a63f57bb65c09da1adc0810c294373f6844
Christian Broberg <cb@cb-pro.local>
2018-12-05 02:54:42 +0100

    Forgot .env load

--- server/query.js
+++ server/query.js
@@ -1,11 +1,8 @@
 require('dotenv').load()
-// get the client
 const mysql = require('mysql2')
-// const mysql = require('mysql')
 
 const { DB_HOST, DB_USER, DATABASE, PASSWORD } = process.env
 
-// create the connection to database
 const connection = mysql.createConnection({
 	host: DB_HOST,
 	user: DB_USER,
@@ -13,7 +10,6 @@ const connection = mysql.createConnection({
 	database: DATABASE
 })
 
-// simple query
 connection.query(
 	'SELECT * FROM `events` WHERE `name` = "Christian Broberg"',
 	function (err, results, fields) {
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').load()
 const express = require('express')
 const mysql = require('mysql2')
 
@@ -13,7 +14,7 @@ const connection = mysql.createConnection({
 const app = express()
 
 app.get('/posts', (req, res) => {
-	connection.connect()
+	// connection.connect()
 
 	connection.query('SELECT * FROM `events` WHERE `name` = "Christian Broberg"', (error, results, fields) => {
 		if (error) throw error
@@ -23,6 +24,6 @@ app.get('/posts', (req, res) => {
 	connection.end()
 })
 // Start the server
-app.listen(3000, () => {
-	console.log('Go to http://localhost:3000/posts to see posts')
+app.listen(3005, () => {
+	console.log('Go to http://localhost:3005/posts to see posts')
 })
