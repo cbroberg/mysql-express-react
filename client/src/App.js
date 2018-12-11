d0ed87171f6f6c15d038953f40754bc44019b12f
Christian Broberg <cb@cb-pro.local>
2018-12-11 01:37:24 +0100

    Backup

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,88 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			events: [],
+			event: {
+				name: 'Peter Hyatt',
+				date: '2018-10-04',
+				birthday: '1',
+				content: 'This is a birthday event'
+			}
+		}
+	}
+	
+	getEvents = () => {
+		fetch('http://localhost:3005/events')
+		.then(response => response.json())
+		.then(response => this.setState({ events: response }))
+		// .then(() => console.log(this.state.events))
+		.catch(error => console.error(error))
+	}
+	
+	addEvent = () => {
+		const { event } = this.state
+		fetch(`http://localhost:3005/events/add/?name=${event.name}&date=${event.date}&birthday=${event.birthday}&content=${event.content}`)
+		.then(this.getEvents)
+		.catch(error => console.error(error))
+		this.setState({
+			event: {
+				name: 'Peter Hyatt',
+				date: '2018-10-04',
+				birthday: '1',
+				content: 'This is a birthday event'
+			} 
+		})
+	}
+
+	componentDidMount() {
+		this.getEvents()
+	}		
+	
+	renderEvents = ({ id, name, date, birthday, content }) => 
+		<div key={id}>
+			<ul>
+				{name} {date} {birthday} {content}
+			</ul>
+		</div>
+
+
+	render() {
+		const { events, event } = this.state
+		return (
+			<div className="App">
+				<div>
+					<input 
+						value={event.name} 
+						placeholder={'Name'}
+						onChange={e => this.setState({event: { ...event, name: e.target.value }})} 
+						/>
+					<input 
+						value={event.date} 
+						placeholder={'Date'}
+						onChange={e => this.setState({event: { ...event, date: e.target.value }})} 
+						/>
+					<input 
+						value={event.birthday} 
+						placeholder={'Birthday? 0/1'}
+						onChange={e => this.setState({event: { ...event, birthday: e.target.value }})} 
+						/>
+					<input 
+						value={event.content} 
+						placeholder={'Content'}
+						onChange={e => this.setState({event: { ...event, content: e.target.value }})} 
+					/>
+					<button onClick={this.addEvent}>
+						Add event
+					</button>
+				</div>
+				{events.map(this.renderEvents)}
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').load()
 const express = require('express')
+const cors = require('cors')
 const mysql = require('mysql2')
 
 const { DB_HOST, DB_USER, DATABASE, PASSWORD } = process.env
@@ -11,7 +12,15 @@ const connection = mysql.createConnection({
 	database: DATABASE
 })
 
+connection.connect((error) => {
+	if (!error) 
+		console.log('Database connection established')
+	else
+		console.log('Database connection failed \n Error : ' + JSON.stringify(error, undefined, 2))
+})
+
 const app = express()
+app.use(cors())
 
 // Standard querY
 const QUERY = {
@@ -19,20 +28,45 @@ const QUERY = {
 	select10: 'SELECT * FROM events LIMIT 0, 10',
 }
 
+app.get('/', (req, res) => {
+	res.redirect('/events')
+})
+
 app.get('/events', (req, res) => {
 	connection.query(QUERY.selectAll, (error, results, fields) => {
-		if (error) throw error
-		res.json(results)
+		if (error) {
+			res.send(error)
+		} else {
+			res.json(results)
+		}
 	})
+})
 
+app.get('/event/:id', (req, res) => {
+	let id = req.params.id
+	connection.query('SELECT * FROM events WHERE id = ?', [id], (error, results, fields) => {
+		if (error) {
+			res.send(error)
+		} else {
+			res.json(results)
+		}
+	})
+})
+
+app.delete('/event/:id', (req, res) => {
+	let id = req.params.id
+	connection.query('DELETE FROM events WHERE id = ?', [id], (error, results, fields) => {
+		if (error) {
+			res.send(error)
+		} else {
+			res.send(`Deleted record with ID: ${id}`)
+		}
+	})
 })
 
 app.get('/events/add', (req, res) => {
 	const { date, name, birthday, content } = req.query
-	console.log(date, name, birthday, content)
-
 	const INSERT_EVENT = `INSERT INTO events (date, name, birthday, content) VALUES('${date}', '${name}', '${birthday}', '${content}')`
-
 	connection.query(INSERT_EVENT, (error, results, fields) => {
 		if (error) {
 			res.send(error)
@@ -42,17 +76,33 @@ app.get('/events/add', (req, res) => {
 	})
 })
 
-
-
-app.get('/events/new', (req, res) => {
-	connection.query("INSERT INTO events (id, date, name, birthday, content) VALUES(6, '18-12-31', 'Nytårsaften', 0, 'Fest')", (error, results, fields) => {
-		if (error) throw error
-		res.json('Values inserted')
+app.post('/events/add', (req, res) => {
+	const { date, name, birthday, content } = req.query
+	const INSERT_EVENT = `INSERT INTO events (date, name, birthday, content) VALUES('${date}', '${name}', '${birthday}', '${content}')`
+	connection.query(INSERT_EVENT, (error, results, fields) => {
+		if (error) {
+			res.send(error)
+		} else {
+			res.send('Event succesfully added!')
+		}
 	})
+})
 
+app.post('/events/add_sp', (req, res) => {
+	const { date, name, birthday, content } = req.query
+	const INSERT_EVENT = `INSERT INTO events (date, name, birthday, content) VALUES('${date}', '${name}', '${birthday}', '${content}')`
+	connection.query(INSERT_EVENT, (error, results, fields) => {
+		if (error) {
+			res.send(error)
+		} else {
+			res.send('Event succesfully added!')
+		}
+	})
 })
 
 // Start the server
 app.listen(3005, () => {
-	console.log('Go to http://localhost:3005/ to see ', Date())
+	console.log('Server up:', Date())
+	console.log('Server running on port 3005')
+	console.log('Go to http://localhost:3005/ to access events')
 })
